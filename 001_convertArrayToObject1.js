df24cf03999c69cdf5f200026a5e54720c07e51a
hornbeek <hornbeek@github.com>
2017-12-30 22:27:38 -0800

    Notes

--- 001_convertArrayToObject1.js
+++ 001_convertArrayToObject1.js
@@ -1,11 +1,9 @@
-
-	
+//referance Manish-Girl Github	
 function transformFirstAndLast(array) {
  const first = array[0];
  const last = array[array.length -1];
  const obj = { [first]:last};
  return obj;
 }
-
 transformFirstAndLast(['Girl', 'Boy', 'Mom', 'Dad']);
 	
